4d47a343ae7d9f3988ea4cb8be1308f67d50b6e0
Arda Kazancı <ardakazanci@anadolu.edu.tr>
2019-11-19 22:33:06 +0300

    FanControlView Added

---
+++ app/src/main/java/com/ardakazanci/customviewfancontroller/FanControlView.kt
@@ -0,0 +1,14 @@
+package com.ardakazanci.customviewfancontroller
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+
+class FanControlView @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : View(context, attrs, defStyleAttr) {
+
+
+}
