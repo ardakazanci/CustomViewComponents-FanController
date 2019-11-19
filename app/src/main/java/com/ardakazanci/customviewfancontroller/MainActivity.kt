61fb4eede90cdcee0b20d85e9cd608093fa56ede
Arda Kazancı <ardakazanci@anadolu.edu.tr>
2019-11-19 22:07:46 +0300

    Initial commit

---
+++ app/src/main/java/com/ardakazanci/customviewfancontroller/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.ardakazanci.customviewfancontroller
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
